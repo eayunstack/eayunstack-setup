6ff6f1b86a2f093ab95959c2afb808319a2d5e12
Dunrong Huang <riegamaths@gmail.com>
2014-09-19 11:08:36 +0800

    setup: reorganize directory
    Signed-off-by: Dunrong Huang <riegamaths@gmail.com>

--- setup.py
+++ setup.py
@@ -10,11 +10,15 @@ setup(
     license='GPLv3',
     keywords='RDO OpenStack',
     entry_points={
-        'cfg': [
-            'role = cfg:make_role',
-            'network = cfg:make_network',
-            'hostname = cfg:make_hostname',
-            'openstack = cfg:make_openstack',
+        'console_scripts': [
+            'es-setup = es_setup.main:main',
         ],
-    }
+
+        'es_setup.cfg': [
+            'role = es_setup.cfg:make_role',
+            'network = es_setup.cfg:make_network',
+            'hostname = es_setup.cfg:make_hostname',
+            'openstack = es_setup.cfg:make_openstack',
+        ],
+    },
 )
